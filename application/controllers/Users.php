6bc697ea2a615b41329db4966eff1dab626468d1
Zxander <alexandurpetrov94@abv.bg>
2017-05-15 16:57:52 +0200

    Revert "Creating Login & Register"
    This reverts commit 04dde8027e858a8ae267cf623210e78da8eeb5e9.

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -7,8 +7,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Users extends CI_Controller
 {
 	
-	public function __construct() {
-		parent::__construct();
-		$this->load->model('users');
+	function __construct()
+	{
+		# code...
 	}
 }
