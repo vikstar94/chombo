5f4ef419745ad2321a1d80f72d6072f32b5e114e
Zxander <alexandurpetrov94@abv.bg>
2017-05-23 23:52:50 +0200

    Chombo logic
    Created a logic for clicking the title of the chombo and gets navigated
    to other page that is for the specific chombo.

--- application/controllers/Chombos.php
+++ application/controllers/Chombos.php
@@ -1,4 +1,5 @@
 <?php
+defined('BASEPATH') OR exit('No direct script access allowed');
 
 /**
 * Chombos controller
@@ -6,8 +7,16 @@
 class Chombos extends CI_Controller
 {
 	
-	function __construct()
-	{
-		# code...
+	public function __construct () {
+		parent::__construct();
+		$this->load->model('chombos_model');
+	}
+
+	public function view($chombo_id) {
+		$this->data['chombo_data'] = $this->chombos_model->chombo_data($chombo_id);
+
+		$this->load->view('templates/header', $this->data);
+		$this->load->view('users/profile');
+		$this->load->view('templates/footer');	
 	}
 }
